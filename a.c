8fba3494d7a288bac8e30ad6aa984fc0ded134db
GJ2025 <3559666579@qq.com>
2025-04-09 09:20:56 -0400

    add read ip from kernel

--- a.c
+++ a.c
@@ -5,7 +5,22 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/mman.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#define  u32 uint32_t
 #include "ring.h"
+
+void print_ip(unsigned int ip) {
+    unsigned int net_ip = htonl(ip);
+
+    unsigned char bytes[4];
+    bytes[0] = (net_ip >> 24) & 0xFF;  // 第一个字节
+    bytes[1] = (net_ip >> 16) & 0xFF;  // 第二个字节
+    bytes[2] = (net_ip >> 8)  & 0xFF;  // 第三个字节
+    bytes[3] = net_ip & 0xFF;           // 第四个字节
+
+    printf("%u.%u.%u.%u\n", bytes[0], bytes[1], bytes[2], bytes[3]);
+}
 
 int main() {
     const char *filename = "/proc/simple_int";
@@ -34,6 +49,12 @@ int main() {
 		printf("error:%s\n", strerror(errno));
 	}else{
 		printf("ring.head(%ld), ring.tail(%ld), ring.mask(%0x)\n", ring->head, ring->tail, ring->mask);
+		if (ring->head != ring->tail){
+			print_ip(ring->ips[ring->head & ring->mask].saddr);	
+			print_ip(ring->ips[ring->head & ring->mask].daddr);
+
+			ring->head++;	
+		}
 	}
 
 
